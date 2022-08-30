2961329b47f44b6b0329056a3522e16ccf6ca721
omnisilica <oxmaverix@gmail.com>
2022-08-30 16:39:23 -0400

    First commit. Start payrollRestService-t project.

---
+++ src/main/java/com/restservice/payrollRestServicet/PayrollRestServiceTApplication.java
@@ -0,0 +1,13 @@
+package com.restservice.payrollRestServicet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollRestServiceTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollRestServiceTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restservice/payrollRestServicet/PayrollRestServiceTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restservice.payrollRestServicet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollRestServiceTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
